733ff14b2e2de4c0234761f616b4ff78280da669
miguel <miguelpi1998@gmail.com>
2024-04-04 18:59:39 +0200

    backend fuera

--- Backend/schemas/sesiones.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
-
-class Sesiones(BaseModel):
-    id: Optional[int]
-    nombre: str
-    fecha: datetime
-    id_cliente: int
