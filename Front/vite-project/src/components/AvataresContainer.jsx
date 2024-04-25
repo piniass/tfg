8f621fc4aa36d71a71fa8b01c63a399c086c6686
miguel <miguelpi1998@gmail.com>
2024-04-25 21:02:19 +0200

    Update AvataresContainer.jsx

--- Front/vite-project/src/components/AvataresContainer.jsx
+++ Front/vite-project/src/components/AvataresContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AvataresContainer = ({ sacarImagen }) => {
-  const ruta = '../../public/';
+  const ruta = '/';
   // const ruta = '/';
   
   const avatares = [
