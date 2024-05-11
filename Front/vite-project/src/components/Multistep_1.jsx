b0fd95344d951613a374ffb18ca811bdb69c3679
miguel <miguelpi1998@gmail.com>
2024-05-11 12:39:24 +0200

    Update Multistep_1.jsx

--- Front/vite-project/src/components/Multistep_1.jsx
+++ Front/vite-project/src/components/Multistep_1.jsx
@@ -14,7 +14,7 @@ export default function Multistep_1(props) {
     <>
         <h2 className='text-xl text-center my-auto text-white '>Introduce tus datos</h2>
         <div className='flex flex-col w-full'>
-            <input type="text" name="nombre" id="nombre" className='w-full p-2 mb-8' placeholder='Introduce tu nombre...'/>
+            <input type="text" name="nombre" id="nombre" className='w-full p-2 mb-10' placeholder='Introduce tu nombre...'/>
             <input type="text" name="apellido" id="apellido" className='w-full p-2' placeholder='Introduce tu apellido...'/>
         </div>
         <button value="Siguiente" className='bg-slate-50 w-full p-2 mt-auto' onClick={handleNext}>Siguiente</button>
