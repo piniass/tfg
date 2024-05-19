73355ca7ccef2876f486cfea022c9fbd3bca7275
miguel <miguelpi1998@gmail.com>
2024-05-19 12:32:36 +0200

    login nuevo

--- Front/vite-project/src/App.jsx
+++ Front/vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserProvider';
 
 // Importa tus componentes
 import PaginaFormulario from './pages/PaginaFormulario';
@@ -9,22 +10,22 @@ import CrearCliente from './pages/CrearCliente';
 import ClienteDetalles from './pages/ClienteDetalles';
 import Login from './pages/Login';
 import PaginaRutinas from './pages/PaginaRutinas';
+
 export default function App() {
   return (
+    <UserProvider>
       <Router>
-          <Routes>
-              {/* Define tus rutas */}
-              <Route path="/" element={<PaginaFormulario />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/tareas" element={<Tareas/>} />
-              <Route path="/registrar" element={<CrearCliente/>} />
-              <Route path="/detalles" element={<ClienteDetalles/>} />
-              <Route path="/login" element={<Login/>} />
-              <Route path="/rutinas" element={<PaginaRutinas/>} />
-
-
-
-          </Routes>
+        <Routes>
+          {/* Define tus rutas */}
+          <Route path="/" element={<PaginaFormulario />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tareas" element={<Tareas />} />
+          <Route path="/registrar" element={<CrearCliente />} />
+          <Route path="/detalles" element={<ClienteDetalles />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/rutinas" element={<PaginaRutinas />} />
+        </Routes>
       </Router>
+    </UserProvider>
   );
 }
--- Front/vite-project/src/components/Formulario.jsx
+++ Front/vite-project/src/components/Formulario.jsx
@@ -1,110 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import qs from 'qs';
-
-
-
+import { useUser } from '../context/UserProvider';
 
 export default function Formulario() {
+  const { setUserId, userId } = useUser();
 
-    const [correo, setCorreo] = useState('');
-    const [contrasenia, setContrasenia] = useState('');
-    const [datosRecibidos, setDatosRecibidos] = useState(null);
-    const [error, setError] = useState(null);
-    const navigate = useNavigate(); 
+  const [correo, setCorreo] = useState('');
+  const [contrasenia, setContrasenia] = useState('');
+  const [datosRecibidos, setDatosRecibidos] = useState(null);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate(); 
 
-    const login = () => {
-        navigate("/login")
-    }
+  const login = () => {
+    navigate("/login");
+  }
 
-    const handleSubmit = async (event) => {
-        console.log("hola")
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
     try {
-        // Realizar la petición POST a la API
-        const url = 'http://127.0.0.1:8000/entrenador/login';
-        const data = {
-            correo: correo,
-            password: contrasenia
-        };
+      // Realizar la petición POST a la API
+      const url = 'http://127.0.0.1:8000/entrenador/login';
+      const data = {
+        correo: correo,
+        password: contrasenia
+      };
 
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            data: qs.stringify(data),
-            url: url,
-        };
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(data),
+        url: url,
+      };
 
-        const res = await axios(options);
+      const res = await axios(options);
 
-        if (res.data.token) {
-            const tiempo = 3600
-            const expTime = new Date(new Date().getTime() + (tiempo * 1000));
+      if (res.data.token) {
+        const tiempo = 3600;
+        const expTime = new Date(new Date().getTime() + (tiempo * 1000));
 
-            // Establecer la cookie
-            document.cookie = `token=${res.data.token}; expires=${expTime.toUTCString()}; path=/`;
+        // Establecer la cookie
+        document.cookie = `token=${res.data.token}; expires=${expTime.toUTCString()}; path=/`;
+        console.log(res.data.entrenador);
+        setUserId(res.data.entrenador.id);
+        // Redirigir a otra página
+        // navigate("/dashboard");
+      }
+      setDatosRecibidos(res.data);
+    } catch (error) {
+      // Manejar errores de la petición
+      setError(error);
+    }
+  };
 
-            // Redirigir a otra página
-            navigate("/dashboard");
-        }
-            setDatosRecibidos(response.data);
-            // Verificar coincidencia entre correo y contraseña ingresados y los datos recibidos
-            /*const coincidencia = response.data.find(entrenador => entrenador.correo === correo && entrenador.password === contrasenia);
-            if (coincidencia) {
-                // Utilizar navigate para navegar a la ruta del dashboard (Home)
-                sessionStorage.setItem("id", coincidencia.id);
-                sessionStorage.setItem("nombre", coincidencia.nombre);
-                sessionStorage.setItem("apellido", coincidencia.apellido);
-                sessionStorage.setItem("foto", coincidencia.avatar);
+  // Efecto para imprimir el userId cuando cambia
+  useEffect(() => {
+    console.log("User ID updated:", userId);
+  }, [userId]);
 
-                console.log(coincidencia.id)
-                navigate('/dashboard');
-            } else {
-                alert('No se encontraron coincidencias.');
-            }*/
-        } catch (error) {
-            // Manejar errores de la petición
-            setError(error);
-        }
-    };
   return (
     <section className='p-8'>
-        <form onSubmit={handleSubmit} className='border-2 w-80 flex flex-col p-8 gap-4 '>
-            <h2 className='text-center text-2xl text-white'>Iniciar Sesion</h2>
+      <form onSubmit={handleSubmit} className='border-2 w-80 flex flex-col p-8 gap-4 '>
+        <h2 className='text-center text-2xl text-white'>Iniciar Sesion</h2>
 
-            <label className='text-white' htmlFor='correo'>Introduce tu correo</label>
-            <input 
-                className='p-2'
-                type="text" 
-                placeholder="Correo electronico"
-                id="correo"
-                name='email'
-                value={correo}
-                onChange={(e) => setCorreo(e.target.value)}
-                />
-            <label htmlFor='contrasenia' className='text-white'>Contraseña</label>
-            <input 
-                className='p-2'
-                type="password" 
-                placeholder="Contraseña"
-                id="contrasenia"
-                onChange={(e) => setContrasenia(e.target.value)}
-                value={contrasenia} />
-            <input 
-                type="submit" 
-                value="Iniciar Sesion"
-                className='rounded cursor-pointer bg-green-600 p-2 text-white'
-                />
-            <input
-                type='button'
-                value='Registrarse'
-                className='rounded cursor-pointer  bg-blue-600 p-2 text-white'
-                onClick={login}
-            />
-        </form>
+        <label className='text-white' htmlFor='correo'>Introduce tu correo</label>
+        <input 
+          className='p-2'
+          type="text" 
+          placeholder="Correo electronico"
+          id="correo"
+          name='email'
+          value={correo}
+          onChange={(e) => setCorreo(e.target.value)}
+        />
+        <label htmlFor='contrasenia' className='text-white'>Contraseña</label>
+        <input 
+          className='p-2'
+          type="password" 
+          placeholder="Contraseña"
+          id="contrasenia"
+          onChange={(e) => setContrasenia(e.target.value)}
+          value={contrasenia} 
+        />
+        <input 
+          type="submit" 
+          value="Iniciar Sesion"
+          className='rounded cursor-pointer bg-green-600 p-2 text-white'
+        />
+        <input
+          type='button'
+          value='Registrarse'
+          className='rounded cursor-pointer bg-blue-600 p-2 text-white'
+          onClick={login}
+        />
+      </form>
     </section>
-    
   );
 }
