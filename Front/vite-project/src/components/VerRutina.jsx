e8f4d1fec069dea528388ca19820b9e2b8492087
miguel <miguelpi1998@gmail.com>
2024-06-03 09:49:42 +0200

    .

--- Front/vite-project/src/components/VerRutina.jsx
+++ Front/vite-project/src/components/VerRutina.jsx
@@ -1,38 +1,67 @@
 import useRutinas from "../hooks/HookRutinas";
 import React, { useEffect, useState } from "react";
 import ModalRutinas from "./ModalRutinas";
+import useEntrenamiento from "../hooks/HookEntrenamiento";
+import TablaEjercicios from "./TablaEjercicios";
+import useEjercicio from "../hooks/HookEjercicio";
 
 export default function VerRutina(props) {
     const { id, id_rutina, id_entrenador, updateState  } = props;
     const { rutinas, getRutinasId } = useRutinas();
     const [rutinaObj, setRutinaObj] = useState(null); // Usar useState para inicializar el estado
-    const [showModal, setModal] = useState(false)
+    const [showModal, setModal] = useState(false);
+    const rutinaNumber = Number(id_rutina)
+    const { entrenamiento, getEntrenamiento } = useEntrenamiento({ rutinaId: rutinaNumber });
+    const [entrenamientoId, setEntrenamientoId] = useState('');
+    const { ejercicio, getEjercicios } = useEjercicio({ entrenamientoId });
 
-    console.log("id usuario: ",id)
+
 
     useEffect(() => {
         if (id) {
             getRutinasId(id_entrenador);
-            // console.log("rutinas ver rutina: ", rutinas)
+            
         }
     }, [id]);
 
+
     useEffect(() => {
-        if (rutinas && rutinas.length > 0) {
-            const rutinaUsada = rutinas.find((rutina) => rutina.id === id_rutina);
-            console.log("Rutina usada: ",rutinaUsada)
-            setRutinaObj(rutinaUsada);
-        }
+        const handleRutinas = () => {
+            if (rutinas && rutinas.length > 0) {
+                const rutinaUsada = rutinas.find((rutina) => rutina.id === id_rutina);
+                console.log("Rutina usada: ", rutinaUsada);
+                setRutinaObj(rutinaUsada);
+            }
+        };
+        handleRutinas();
+        getEntrenamiento();
     }, [rutinas]);
+    
+
+    useEffect(() => {
+        if (entrenamientoId) {
+            getEjercicios();
+            console.log(ejercicio)
+        }
+    }, [entrenamientoId]);
+
+    useEffect(() => {
+        setEntrenamientoId('');
+    }, [id_rutina]);
 
     const openModal = () => {
         showModal ? setModal(false) : setModal(true)
     }
 
+    const getEntrenamientoId = (id) => {
+        console.log("click: ",entrenamientoId)
+        setEntrenamientoId(id);
+    };
+
     return (
-        <div className="p-4">
+        <div className="p-4 flex flex-col gap-4">
             {rutinaObj ? <p>Nombre de la rutina: {rutinaObj.nombre}</p> : <p>No hay una rutina asignada</p>}
-            <button className="mt-2 bg-transparent border-red-600" onClick={openModal}>{rutinaObj ? (<p>Cambiar rutina</p>) : <p>Agregar rutina</p>}</button> 
+            <button className=" bg-transparent border-red-600" onClick={openModal}>{rutinaObj ? (<p>Cambiar rutina</p>) : <p>Agregar rutina</p>}</button> 
             {showModal && <ModalRutinas rutinas={rutinas} id_usuario={id} id_rutina={id_rutina} getRutinas={getRutinasId} id_entrenador={id_entrenador} updateState={updateState} setModal={setModal}/>}
             
             {/* <ul>
@@ -40,6 +69,21 @@ export default function VerRutina(props) {
                     return <li key={rutina.id}>{rutina.nombre}</li>; // Añadir el return y la key
                 })}
             </ul> */}
+            {entrenamiento.length > 0 ? 
+            <div className="flex gap-2 w-full">{ entrenamiento.map((training) => 
+                <button key={training.id} onClick={() => getEntrenamientoId(training.id)}>{training.nombre}</button>)
+                }
+            </div>
+                : <p>No hay ningun entreamiento creado todavía.</p>}
+
+                {entrenamientoId  && ejercicio.length > 0 ?(
+                    <TablaEjercicios entrenamientoId={entrenamientoId} ejercicio={ejercicio} getEjercicios={getEjercicios}/>
+                ) : (
+                    <p className='p-4'>Selecciona un entrenamiento</p>
+                )}
+            
         </div>
+
+            
     );
 }
